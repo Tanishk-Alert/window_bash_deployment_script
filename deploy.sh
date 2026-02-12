fa608bd8d1c531bd33e0d2589ef63facc3a42bf2
Tanishk-Alert <tanishkmarothiya@alertenterprise.com>
2026-02-12 09:49:47 +0530

    new coe

--- deploy.sh
+++ deploy.sh
@@ -1345,17 +1345,17 @@ main() {
         exit 0
     fi
     create_dirs
-    #stop_services
-    #logoff_other_sessions
-    #backup
-    # download_build
-    #extract_zip
-    #copy_env_configs
-    #update_environment_conf
-    ##setup_keystore
-    #uiSetup
-    #applicationStart
-    #validate
+    stop_services
+    logoff_other_sessions
+    backup
+    download_build
+    extract_zip
+    copy_env_configs
+    update_environment_conf
+    setup_keystore
+    uiSetup
+    applicationStart
+    validate
     flyway_run
 }
 
